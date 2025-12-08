b8acf8588ff749e0ea971ce5c0b3f1e7a87bae3a
velu <mayainteriors.in@gmail.com>
2025-12-08 21:56:08 +0530

    Sync local updates to Render

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,4 @@
+﻿import "dotenv/config";
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
@@ -28,7 +29,7 @@ app.use((req, res, next) => {
       }
 
       if (logLine.length > 80) {
-        logLine = logLine.slice(0, 79) + "…";
+        logLine = logLine.slice(0, 79) + "â€¦";
       }
 
       log(logLine);
